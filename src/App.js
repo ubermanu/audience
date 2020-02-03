bab6aa0d69f034fd2c958fb90fe92dd4b66503e3
ubermanu <manu@paca.happy-dev.fr>
2020-02-03 17:34:22 +0000

    Add custom style to emoji buttons

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import styled from "styled-components";
 import EmojiButton from "./EmojiButton";
 import sounds from "./sounds.json";
 
-const FlexContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+const Background = styled.div`
   background: #111;
   min-height: 100vh;
-  width: 100vw;
+  width: 100%;
+`;
+
+const FlexContainer = styled.div`
+  max-width: 100%;
+  padding: 1rem;
 `;
 
 const OneFourthCell = styled.div`
   width: 25%;
   overflow-x: hidden;
+  display: inline-block;
+  margin-bottom: 1rem;
+  text-align: center;
 `;
 
 const App = () => (
-  <FlexContainer>
-    {sounds.map((item, i) => (
-      <OneFourthCell key={i}>
-        <EmojiButton {...item} />
-      </OneFourthCell>
-    ))}
-  </FlexContainer>
+  <Background>
+    <FlexContainer>
+      {sounds.map((item, i) => (
+        <OneFourthCell key={i}>
+          <EmojiButton {...item} />
+        </OneFourthCell>
+      ))}
+    </FlexContainer>
+  </Background>
 );
 
 export default App;
--- src/EmojiButton.js
+++ src/EmojiButton.js
@@ -1,8 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
-const EmojiButton = ({ emoji, src }) => {
+const Button = styled.div`
+  border-radius: 50%;
+  display: inline-block;
+  cursor: pointer;
+  text-align: center;
+  font-size: 10vw;
+`;
+
+const Emoji = styled.div`
+  font-size: 1em;
+`;
+
+const Name = styled.div`
+  font-size: 0.33em;
+  font-family: "Arial", sans-serif;
+  color: white;
+  margin-top: 1em;
+  text-shadow: 0 0 10px black;
+`;
+
+const EmojiButton = ({ emoji, name, src }) => {
   const audio = new Audio(`media/${src}`);
-  return <button onClick={() => audio.play()}>{emoji}</button>;
+  return (
+    <Button onClick={() => audio.play()}>
+      <Emoji>{emoji}</Emoji>
+      <Name>{name}</Name>
+    </Button>
+  );
 };
 
 export default EmojiButton;
