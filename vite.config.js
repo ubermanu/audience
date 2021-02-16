5ce63f2326270ef29617174379993736247e3a53
ubermanu <e.vodor@gmail.com>
2021-02-16 23:15:04 +0100

    Fix production build and add sw

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import svelte from "rollup-plugin-svelte-hot";
+import svelte from "rollup-plugin-svelte";
+import svelteHot from "rollup-plugin-svelte-hot";
+import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./src/manifest.json";
+
+// Note: SvelteHOT builds the wrong output in index.html
+const ViteSvelte = process.env.NODE_ENV === "production" ? svelte : svelteHot;
 
 /**
  * @type { import('vite').UserConfig }
  */
 export default {
-  plugins: [svelte()],
-  rollupDedupe: ["svelte"],
+  plugins: [ViteSvelte(), VitePWA({ manifest })],
 };
