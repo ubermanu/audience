c5b2e4ff459c53cb160957546bbee44ebbb99997
ubermanu <manu@paca.happy-dev.fr>
2021-06-05 11:41:39 +0200

    Update deps, remove workbox file

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  globDirectory: "./build",
-  globPatterns: ["**/*.{html,js,css,ogg,png}"],
-  swDest: "build/sw.js",
-  clientsClaim: "true",
-  skipWaiting: "true",
-  sourcemap: false,
-};
