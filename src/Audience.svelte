e73899ec1fac9326e535a5697aaca27b0ed90c9a
ubermanu <manu@paca.happy-dev.fr>
2023-03-16 12:17:55 +0100

    cleanup

--- src/Audience.svelte
+++ src/Audience.svelte
@@ -1,38 +1,8 @@
 <script>
-  import "normalize.css";
-  import sounds from "./sounds.json";
-  import EmojiButton from "./EmojiButton.svelte";
+  import 'normalize.css'
+  import sounds from './sounds.json'
+  import EmojiButton from './components/EmojiButton.svelte'
 </script>
-
-<style>
-    main {
-        background: #ecf0f1;
-        min-height: 100vh;
-        width: 100%;
-    }
-
-    @media (prefers-color-scheme: dark) {
-        main {
-            background: #34495e;
-        }
-    }
-
-    ul {
-        list-style: none;
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(4rem, 1fr));
-        grid-gap: 2rem;
-        padding: 2rem;
-        margin: 0;
-    }
-
-    li {
-        overflow-x: hidden;
-        display: inline-block;
-        margin-bottom: 1rem;
-        text-align: center;
-    }
-</style>
 
 <main>
   <ul>
@@ -43,3 +13,33 @@
     {/each}
   </ul>
 </main>
+
+<style>
+  main {
+    background: #ecf0f1;
+    min-height: 100vh;
+    width: 100%;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    main {
+      background: #34495e;
+    }
+  }
+
+  ul {
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(4rem, 1fr));
+    grid-gap: 2rem;
+    padding: 2rem;
+    margin: 0;
+  }
+
+  li {
+    overflow-x: hidden;
+    display: inline-block;
+    margin-bottom: 1rem;
+    text-align: center;
+  }
+</style>
