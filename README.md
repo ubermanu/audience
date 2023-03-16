ebf1fe9e629a28f09d2807bfc58f972516570990
ubermanu <manu@paca.happy-dev.fr>
2023-03-16 12:17:12 +0100

    update deps

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Audience
 
+<img src="public/applause.svg" width="100" height="100" alt=""/>
+
 Audience is an open-source crowd reaction PWA.
 
 ### Usage
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import svelte from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 import manifest from './src/manifest.json'
 
-export default {
+export default defineConfig({
   plugins: [svelte(), VitePWA({ manifest })],
-}
+})
