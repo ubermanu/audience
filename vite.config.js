ae04162912a866d6074fac87b12eaca07be26844
ubermanu <manu@paca.happy-dev.fr>
2024-12-03 16:58:46 +0100

    feat: update deps + tw beta

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite'
 import manifest from './src/manifest.json'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     sveltekit(),
     SvelteKitPWA({
       strategies: 'generateSW',
